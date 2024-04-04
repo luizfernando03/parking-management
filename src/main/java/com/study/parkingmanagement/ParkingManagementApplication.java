57111c617d584e00dfd0c915673b3617cf2cf7eb
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-04 19:58:33 -0300

    Criando Projeto

---
+++ src/main/java/com/study/parkingmanagement/ParkingManagementApplication.java
@@ -0,0 +1,21 @@
+package com.study.parkingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ParkingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index(){
+		return "Ola Mundo";
+	}
+
+}
---
+++ src/test/java/com/study/parkingmanagement/ParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.parkingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
