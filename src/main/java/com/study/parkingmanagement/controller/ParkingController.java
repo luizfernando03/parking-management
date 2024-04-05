73499ebeb359dcef4c57948191f425afad3bdc1c
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-05 10:31:33 -0300

    dto

--- src/main/java/com/study/parkingmanagement/controller/ParkingController.java
+++ src/main/java/com/study/parkingmanagement/controller/ParkingController.java
@@ -1,4 +1,17 @@
 package com.study.parkingmanagement.controller;
 
+import com.study.parkingmanagement.repository.ParkingRepository;
+import com.study.parkingmanagement.service.ParkingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/parking")
 public class ParkingController {
+
+    final ParkingService parkingService;
+
+    
 }
