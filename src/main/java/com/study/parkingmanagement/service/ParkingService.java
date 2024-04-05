5284a3c50b31933ffa806075a75a0a273e56fa53
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-05 10:19:58 -0300

    subindo dados na Service

--- src/main/java/com/study/parkingmanagement/service/ParkingService.java
+++ src/main/java/com/study/parkingmanagement/service/ParkingService.java
@@ -1,7 +1,16 @@
 package com.study.parkingmanagement.service;
 
+import com.study.parkingmanagement.repository.ParkingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingService {
+
+    @Autowired
+     final ParkingRepository parkingRepository;
+
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
 }
