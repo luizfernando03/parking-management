967bfc547156aa0435a77e7bd84d94ec3dbb4b78
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-05 09:58:33 -0300

    Subindo dados para a Model

--- src/main/java/com/study/parkingmanagement/model/ParkingModel.java
+++ src/main/java/com/study/parkingmanagement/model/ParkingModel.java
@@ -1,4 +1,33 @@
 package com.study.parkingmanagement.model;
 
-public class ParkingModel {
+
+import lombok.Data;
+import org.yaml.snakeyaml.util.EnumUtils;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_PARKING")
+public class ParkingModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+   @Column (nullable = false, unique = true, length = 10)
+   private String numeroDaVaga;
+   @Column (nullable = false, unique = true, length = 7)
+   private String PlacaDoCarro;
+   @Column(nullable = false,length = 20)
+    private String modeloDoCarro;
+   @Column(nullable = false,length = 50)
+    private String nomeDono;
+   @Column(nullable = false )
+    private LocalDateTime dataDaEntrada;
+
+
 }
