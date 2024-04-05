8e494f3431f73aad548e1608b6a75383c6d830f9
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-04-05 10:13:34 -0300

    Criando Service

--- src/main/java/com/study/parkingmanagement/repository/ParkingRepository.java
+++ src/main/java/com/study/parkingmanagement/repository/ParkingRepository.java
@@ -1,4 +1,12 @@
 package com.study.parkingmanagement.repository;
 
-public interface ParkingRepository {
+import com.study.parkingmanagement.model.ParkingModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<ParkingModel, UUID> {
+
 }
---
+++ src/main/java/com/study/parkingmanagement/service/ParkingService.java
@@ -0,0 +1,7 @@
+package com.study.parkingmanagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingService {
+}
